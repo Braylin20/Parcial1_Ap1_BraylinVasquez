5d84580ab71b39772a4eaf6828e82526dcd9eb1e
Braylin <braylinvasquez20@gmail.com>
2024-02-12 18:44:14 -0400

    Injectando el services y el contexto

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using Parcial1_Ap1_BraylinVasquez.Components;
+using Parcial1_Ap1_BraylinVasquez.Dal;
+using Parcial1_Ap1_BraylinVasquez.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
+builder.Services.AddDbContext<Contexto>(options=> options.UseSqlite(ConStr));
+
+builder.Services.AddScoped<MetasServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
