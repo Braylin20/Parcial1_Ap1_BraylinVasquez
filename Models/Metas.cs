8391efb75c32cfdceae149e1859fb997dd2ca1c8
Braylin <braylinvasquez20@gmail.com>
2024-02-12 18:42:35 -0400

    Sacando la carpeta de models de Components

---
+++ Models/Metas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_BraylinVasquez.Models
+{
+    public class Metas
+    {
+        [Key]
+        public int MetaId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime? Fecha { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage = "Debe ser mayor a 0")]
+        public double? Monto { get; set; }
+    }
+}
